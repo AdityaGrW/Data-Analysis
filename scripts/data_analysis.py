50785d304ad3dc530f74b1ae88f877756878a9e4
Mr. Joker <148346641+AdityaGrW@users.noreply.github.com>
2025-03-24 14:14:58 +0530

    data_analysis.py

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,6 +1,18 @@
+# data_analysis.ipynb
 import pandas as pd
 
+# Load cleaned data
+file_path = 'data/cleaned/nba_cleaned_data.csv'
+data = pd.read_csv(file_path)
+
+# Function to analyze data
 def analyze_data(data):
-    # Replace with your data analysis logic (diff. for every one)
-    analysis_results = data.describe()
-    return analysis_results
+    # Example analysis: summary statistics
+    summary_stats = data.describe()
+    return summary_stats
+
+# Analyze the data
+analysis_results = analyze_data(data)
+
+# Display analysis results
+analysis_results
